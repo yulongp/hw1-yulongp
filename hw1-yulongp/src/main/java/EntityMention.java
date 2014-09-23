4f134f8f57eb7382447bf2fd182cd0d03b2417bb
yulongp <kaiser1943@126.com>
2014-09-23 14:31:48 -0400

    Add type system descriptor

---
+++ hw1-yulongp/src/main/java/EntityMention.java
@@ -0,0 +1,166 @@
+
+
+/* First created by JCasGen Tue Sep 23 14:19:56 EDT 2014 */
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** mention of an entity
+ * Updated by JCasGen Tue Sep 23 14:19:56 EDT 2014
+ * XML source: /home/paul/git/hw1-yulongp/hw1-yulongp/src/main/resources/typeSystemDescriptor.xml
+ * @generated */
+public class EntityMention extends Annotation {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(EntityMention.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected EntityMention() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param addr low level Feature Structure reference
+   * @param type the type of this Feature Structure 
+   */
+  public EntityMention(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public EntityMention(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public EntityMention(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: id
+
+  /** getter for id - gets id of this mention
+   * @generated
+   * @return value of the feature 
+   */
+  public int getId() {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_id == null)
+      jcasType.jcas.throwFeatMissing("id", "EntityMention");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((EntityMention_Type)jcasType).casFeatCode_id);}
+    
+  /** setter for id - sets id of this mention 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setId(int v) {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_id == null)
+      jcasType.jcas.throwFeatMissing("id", "EntityMention");
+    jcasType.ll_cas.ll_setIntValue(addr, ((EntityMention_Type)jcasType).casFeatCode_id, v);}    
+   
+    
+  //*--------------*
+  //* Feature: start
+
+  /** getter for start - gets start position of this entity mention
+   * @generated
+   * @return value of the feature 
+   */
+  public int getStart() {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_start == null)
+      jcasType.jcas.throwFeatMissing("start", "EntityMention");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((EntityMention_Type)jcasType).casFeatCode_start);}
+    
+  /** setter for start - sets start position of this entity mention 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setStart(int v) {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_start == null)
+      jcasType.jcas.throwFeatMissing("start", "EntityMention");
+    jcasType.ll_cas.ll_setIntValue(addr, ((EntityMention_Type)jcasType).casFeatCode_start, v);}    
+   
+    
+  //*--------------*
+  //* Feature: end
+
+  /** getter for end - gets end position of this entity mention
+   * @generated
+   * @return value of the feature 
+   */
+  public int getEnd() {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_end == null)
+      jcasType.jcas.throwFeatMissing("end", "EntityMention");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((EntityMention_Type)jcasType).casFeatCode_end);}
+    
+  /** setter for end - sets end position of this entity mention 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setEnd(int v) {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_end == null)
+      jcasType.jcas.throwFeatMissing("end", "EntityMention");
+    jcasType.ll_cas.ll_setIntValue(addr, ((EntityMention_Type)jcasType).casFeatCode_end, v);}    
+   
+    
+  //*--------------*
+  //* Feature: content
+
+  /** getter for content - gets content of the entity mention
+   * @generated
+   * @return value of the feature 
+   */
+  public String getContent() {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_content == null)
+      jcasType.jcas.throwFeatMissing("content", "EntityMention");
+    return jcasType.ll_cas.ll_getStringValue(addr, ((EntityMention_Type)jcasType).casFeatCode_content);}
+    
+  /** setter for content - sets content of the entity mention 
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setContent(String v) {
+    if (EntityMention_Type.featOkTst && ((EntityMention_Type)jcasType).casFeat_content == null)
+      jcasType.jcas.throwFeatMissing("content", "EntityMention");
+    jcasType.ll_cas.ll_setStringValue(addr, ((EntityMention_Type)jcasType).casFeatCode_content, v);}    
+  }
+
+    
---
+++ hw1-yulongp/src/main/java/EntityMention_Type.java
@@ -0,0 +1,176 @@
+
+/* First created by JCasGen Tue Sep 23 14:19:56 EDT 2014 */
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.tcas.Annotation_Type;
+
+/** mention of an entity
+ * Updated by JCasGen Tue Sep 23 14:19:56 EDT 2014
+ * @generated */
+public class EntityMention_Type extends Annotation_Type {
+  /** @generated 
+   * @return the generator for this type
+   */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /** @generated */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (EntityMention_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = EntityMention_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new EntityMention(addr, EntityMention_Type.this);
+  			   EntityMention_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new EntityMention(addr, EntityMention_Type.this);
+  	  }
+    };
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = EntityMention.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("EntityMention");
+ 
+  /** @generated */
+  final Feature casFeat_id;
+  /** @generated */
+  final int     casFeatCode_id;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public int getId(int addr) {
+        if (featOkTst && casFeat_id == null)
+      jcas.throwFeatMissing("id", "EntityMention");
+    return ll_cas.ll_getIntValue(addr, casFeatCode_id);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setId(int addr, int v) {
+        if (featOkTst && casFeat_id == null)
+      jcas.throwFeatMissing("id", "EntityMention");
+    ll_cas.ll_setIntValue(addr, casFeatCode_id, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_start;
+  /** @generated */
+  final int     casFeatCode_start;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public int getStart(int addr) {
+        if (featOkTst && casFeat_start == null)
+      jcas.throwFeatMissing("start", "EntityMention");
+    return ll_cas.ll_getIntValue(addr, casFeatCode_start);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setStart(int addr, int v) {
+        if (featOkTst && casFeat_start == null)
+      jcas.throwFeatMissing("start", "EntityMention");
+    ll_cas.ll_setIntValue(addr, casFeatCode_start, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_end;
+  /** @generated */
+  final int     casFeatCode_end;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public int getEnd(int addr) {
+        if (featOkTst && casFeat_end == null)
+      jcas.throwFeatMissing("end", "EntityMention");
+    return ll_cas.ll_getIntValue(addr, casFeatCode_end);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setEnd(int addr, int v) {
+        if (featOkTst && casFeat_end == null)
+      jcas.throwFeatMissing("end", "EntityMention");
+    ll_cas.ll_setIntValue(addr, casFeatCode_end, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_content;
+  /** @generated */
+  final int     casFeatCode_content;
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @return the feature value 
+   */ 
+  public String getContent(int addr) {
+        if (featOkTst && casFeat_content == null)
+      jcas.throwFeatMissing("content", "EntityMention");
+    return ll_cas.ll_getStringValue(addr, casFeatCode_content);
+  }
+  /** @generated
+   * @param addr low level Feature Structure reference
+   * @param v value to set 
+   */    
+  public void setContent(int addr, String v) {
+        if (featOkTst && casFeat_content == null)
+      jcas.throwFeatMissing("content", "EntityMention");
+    ll_cas.ll_setStringValue(addr, casFeatCode_content, v);}
+    
+  
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	 * @generated
+	 * @param jcas JCas
+	 * @param casType Type 
+	 */
+  public EntityMention_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_id = jcas.getRequiredFeatureDE(casType, "id", "uima.cas.Integer", featOkTst);
+    casFeatCode_id  = (null == casFeat_id) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_id).getCode();
+
+ 
+    casFeat_start = jcas.getRequiredFeatureDE(casType, "start", "uima.cas.Integer", featOkTst);
+    casFeatCode_start  = (null == casFeat_start) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_start).getCode();
+
+ 
+    casFeat_end = jcas.getRequiredFeatureDE(casType, "end", "uima.cas.Integer", featOkTst);
+    casFeatCode_end  = (null == casFeat_end) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_end).getCode();
+
+ 
+    casFeat_content = jcas.getRequiredFeatureDE(casType, "content", "uima.cas.String", featOkTst);
+    casFeatCode_content  = (null == casFeat_content) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_content).getCode();
+
+  }
+}
+
+
+
+    
